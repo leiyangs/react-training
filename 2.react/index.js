118efe10bf6e110c686fb5a1fda9aa12f9105477
yanglei <yanglei@shjinjia.com>
2020-04-17 17:44:06 +0800

    setState

--- 2.react/index.js
+++ 2.react/index.js
@@ -1,24 +1,51 @@
-class Counter {
-  constructor() {
-    this.state = {number: 0}
+class Component {
+  constructor(props) {
+    this.props = props;
   }
+  // DOM模板字符串转为真实DOM元素
   createDomFromDomString(domString) {
     let div = document.createElement('div');
     div.innerHTML = domString;
     return div.children[0];
   }
-  add() {
-    this.state = {number: this.state.number+1};
+  // 状态变化重新创建dom并替换
+  setState(particleState) {
+    this.state = Object.assign(this.state, particleState);
     let oldElement = this.domElement;
-    let newElement = this.render();
+    let newElement = this.renderElement();
     oldElement.parentElement.replaceChild(newElement, oldElement);
   }
-  render() {
-    this.domElement = this.createDomFromDomString(`<button id="count-app">${this.state.number}</button>`);
-    this.domElement.addEventListener('click', this.add.bind(this))
+  // 添加事件
+  renderElement() {
+    this.domElement = this.createDomFromDomString(this.render());
+    // 绑定事件的两种方式
+    // this.domElement.addEventListener('click', this.add.bind(this))
     // this.domElement.addEventListener('click', () => {
     //   this.add() 
     // })
+    // 使DOM元素de component变为this
+    this.domElement.component = this;
     return this.domElement;
+  }
+  mount(container) {
+    container.appendChild(this.renderElement());
+  }
+}
+
+window.trigger = function (event, method) {
+  // event.target.component == <button id="count-app" onclick="trigger(event, 'add')">${this.props.name}${this.state.number}</button>
+  event.target.component[method].call(event.target.component, event);
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {number: 0};
+  }
+  add(event) {
+    this.setState({number: this.state.number+1});
+  }
+  render() {
+    return `<button id="count-app" onclick="trigger(event, 'add')">${this.props.name}${this.state.number}</button>`
   }
 }
