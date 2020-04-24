253b618b632067b5153c233368e004e5fb812d44
leiyangs <15135210985@163.com>
2020-04-24 18:08:15 +0800

    getSnapshotBeforeUpdate

--- 3.react/src/index.js
+++ 3.react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LifeCycle from './components/lifeCycle';
+import LifeCycle from './components/getDerivedStateFromProps';
 import Counter from './components/NewLifeCycle';
 
 // let element = React.createElement('span', {id: 'id', className: 'class', style: {color: 'red'}}, 11)
