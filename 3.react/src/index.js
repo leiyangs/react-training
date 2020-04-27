c0b507702c78b42501aa881711fa71763d45db21
leiyangs <15135210985@163.com>
2020-04-27 18:17:46 +0800

    context

--- 3.react/src/index.js
+++ 3.react/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import LifeCycle from './components/getDerivedStateFromProps';
 import Counter from './components/NewLifeCycle';
 import GetSnapshotBeforeUpdate from './components/getSnapshotBeforeUpdate';
+import OldContext from './context/OldContext';
 
 // let element = React.createElement('span', {id: 'id', className: 'class', style: {color: 'red'}}, 11)
 ReactDOM.render(
-  <GetSnapshotBeforeUpdate/>,
+  <OldContext/>,
   document.getElementById('root')
 );
