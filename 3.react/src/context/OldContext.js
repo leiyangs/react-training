3c4196eb4c232fe2a39a76e055ece4906df43178
leiyangs <15135210985@163.com>
2020-04-29 15:16:20 +0800

    context

--- 3.react/src/context/OldContext.js
+++ 3.react/src/context/OldContext.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 
+class Header extends Component {
+  render() {
+    return <div style={{border: '1px solid green', padding: '15px'}}>
+      Header
+      <Title></Title>
+    </div>
+  }
+}
+class Title extends Component {
+  // 表示指定要获取那些上下文对象
+  static childContextTypes = {
+    color: 'string',
+    setColor: 'function'
+  }
+  render() {
+    return <div style={{border: '1px solid blue', padding: '15px', color: this.context.color}}>
+      Title
+    </div>
+  }
+}
+class Main extends Component {
+  render() {
+    return <div style={{border: '1px solid orange', padding: '15px'}}>
+      Main
+      <Content></Content>
+    </div>
+  }
+}
+class Content extends Component {
+  render() {
+    return <div style={{border: '1px solid black', padding: '15px'}}>
+      Content
+    </div>
+  }
+}
 export default class OldContext extends Component {
+  // 定义子上下文对象的属性和类型
+  static childContextTypes = {
+    color: 'string',
+    setColor: 'function'
+  }
+  // 返回或者说定义真正的上下文
+  getChildContext() {
+    return {
+      color: this.state.color,
+      setColor: this.setColor
+    }
+  }
+  constructor(props) {
+    super(props);
+    this.state = { color: 'gray' }
+  }
   render() {
     return (
-      <div>
+      <div style={{border: '1px solid red', padding: '15px'}}>
         <Header>
           <Title></Title>
         </Header>
