7b4895b7d12cf1e93413a6ca4977ab359dc16f4d
leiyangs <15135210985@163.com>
2020-05-10 20:53:17 +0800

    memo

--- 3.react/src/pure/Pure.js
+++ 3.react/src/pure/Pure.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react' // { Component, PureComponent }
 import PureComponent from './PureComponent'
 // PureComponent 只渲染属性改变的组件，不使用的话Title组件也会一直重新渲染 pure纯的
 
-class Title extends PureComponent {
+// 类组件的处理方式PureComponent
+class Title1 extends PureComponent {
   render() {
     console.log('Title')
     return <div>{this.props.title}</div>
+  }
+}
+// 函数组件的处理方式memo(备忘录)
+function Title(props) {
+  console.log('Title')
+  return <div>{props.title}</div>
+}
+Title = React.memo(Title);
+// memo的两种实现
+function memo1(FuncComponent) {
+  return class  extends PureComponent {
+    render() {
+      return <FuncComponent {...this.props}/>
+    }
+  }
+}
+function memo2(FuncComponent) {
+  return class  extends PureComponent {
+    render() {
+      return FuncComponent(this.props)
+    }
   }
 }
 
@@ -13,7 +35,7 @@ class Counter extends PureComponent  {
   render() {
     console.log('Counter')
     return <div>{this.props.number}</div>
-  }
+  }s
 }
 
 export default class App extends PureComponent {
