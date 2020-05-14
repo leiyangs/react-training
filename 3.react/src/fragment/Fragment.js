74ef96d6ef29bd60f08d7280f58466a174b35254
leiyangs <15135210985@163.com>
2020-05-14 18:13:47 +0800

    fragment

---
+++ 3.react/src/fragment/Fragment.js
@@ -0,0 +1,16 @@
+// 片段
+import React, { Component } from 'react'
+
+/**
+ * React中一个常见的模式是为一个组件返回多个元素
+ * 片段(Fragment)可以将子元素列表添加到一个分组中，并且不会在DOM中增加额外节点
+ */
+export default class Fragment extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
