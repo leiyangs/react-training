b129f56769cf2f084cf9f2d95f6a0242ffa84016
leiyangs <15135210985@163.com>
2020-05-18 09:50:12 +0800

    redux

--- 4.redux/src/index.js
+++ 4.redux/src/index.js
@@ -1,8 +1,10 @@
+// redux和react没有任何关系，这里没有react也可以使用redux
+
 import { createStore } from 'redux'
 let initState = 0; // 声明state 的默认值
 const INCREMENT = 'INCREMENT'; // 定义+的type
 const DECREMENT = 'DECREMENT'; // 定义-的type
-// 在redux中动作是有规定的，规定必须有一个不为undefined的type属性，用来表示动作类型 
+// 在redux中动作是有规定的，规定必须有一个不为undefined的type属性，用来表示动作类型
 function reducer(state=initState, action) {
   switch (action.type) {
     case INCREMENT:
@@ -26,9 +28,9 @@ function render() {
 render();
 store.subscribe(render); // subscribe订阅 每次store发生变化执行render
 
-incrementBtn.addEventListener('click', function (){
+incrementBtn.addEventListener('click', function () {
   store.dispatch({type: INCREMENT});
 })
-decrementBtn.addEventListener('click', function (){
+decrementBtn.addEventListener('click', function () {
   store.dispatch({type: DECREMENT});
 })
