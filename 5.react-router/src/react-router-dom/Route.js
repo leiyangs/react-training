812b52612c2733d66a44a5a036797549831f1f32
leiyangs <15135210985@163.com>
2020-05-28 15:46:45 +0800

    react-router-dom用法/hashrouter原理

--- 5.react-router/src/react-router-dom/Route.js
+++ 5.react-router/src/react-router-dom/Route.js
@@ -5,7 +5,7 @@ export default class Route extends Component {
   static contextType = Context;
   render() {
     let { pathname } = this.context.location;
-    let { path='/', component: Component, exact=false } = this.props;
+    let { path='/', component: Component, exact=false } = this.props; //<Route path="/" component={Home} exact></Route>解构组件中传入的参数
     if(pathname === path) {
       return <Component/>
     }
