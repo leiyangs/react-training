97bd0535923b9037343c3ffe69c03aa614b82c11
leiyangs <15135210985@163.com>
2020-06-01 18:29:01 +0800

    实现Route中的render

--- 5.react-router/src/components/Protected.js
+++ 5.react-router/src/components/Protected.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Redirect } from '../react-router-dom'
 // rest = {path:profile,exact:true}
 export default function Protected({component: Component, ...rest}) {
   return (
     <Route {...rest} render={
       // 如果是先访问其他页面，然后登录后自动跳转到from也就是之前访问的页面
-      props => localStorage.getItem('islogin')?<Component {...props}/>:<Redirect to={{pathname:'/login', state:{from:props.location.pathname}}}></Redirect>
+      props => localStorage.getItem('islogin')?<Component {...props}/>:<Redirect to={{pathname:'/login', state:{from:props.location.pathname}}}/>
     }/>
   )
 }
