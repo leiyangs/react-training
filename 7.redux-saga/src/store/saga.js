40059030b6a61b8ba30d48ba90fc75148dc6ef8e
leiyangs <15135210985@163.com>
2020-06-04 21:40:02 +0800

    声明式effect

--- 7.redux-saga/src/store/saga.js
+++ 7.redux-saga/src/store/saga.js
@@ -1,16 +1,30 @@
-import { all, takeEvery, delay, put } from 'redux-saga/effects'
+import { all, takeEvery, delay, put, call,apply, cps } from 'redux-saga/effects'
 import * as types from './action-types'
-export function* helloSaga() {
+function* helloSaga() {
   console.log('hello saga');
   yield 1;
   console.log('hello1');
   yield 2;
   console.log('hello2')
 }
-
-export function* incrementAsync() {
+// 模拟node的readfile
+function readfile(filename,callback) {
+  setTimeout(() => {
+    callback(null,filename); // cps(err, res) 所以第一个值放null
+  },1000)
+}
+// cps 用来调用node类型的方法 也就是回调 call和apply只能调用promise 不支持回调
+function* readAsync() {
+  let content = yield cps(readfile, 'README.md');
+  console.log(content)
+}
+function* incrementAsync() {
   // 会返回一个Promise 执行完把resolve的值返回
+  // let obj = {name: 'yang'}
   let msg = yield delay(1000);
+  // 给delay绑定this的写法
+  // let msg = yield call([obj,delay], 1000);
+  // let msg = yield apply(obj,delay, [1000]);
   console.log(msg)
   // 再次向仓库派发INCREMENT动作
   yield put({type: types.INCREMENT})
@@ -18,13 +32,14 @@ export function* incrementAsync() {
 // rootSaga用来阻止和调用其他saga的generator
 // 监听saga 监听向仓库派发的动作，如果监听到动作会通知worker执行
 // workersaga 是真正执行任务的saga 
-export function* watchIncrementAsync() {
+function* watchIncrementAsync() {
   // 监听每一次的ASYNC_INCREMENT动作,每当向仓库派发这个动作时，就会调用另一个worker saga 执行任务
   yield takeEvery(types.ASYNC_INCREMENT, incrementAsync);
 }
 export default function* rootSaga() {
   yield all([ // 等待全部执行
     helloSaga(),
-    watchIncrementAsync()
+    watchIncrementAsync(),
+    readAsync()
   ])
 }
--- 7.redux-saga/src/store/sagas.spec.js
+++ 7.redux-saga/src/store/sagas.spec.js
@@ -1,15 +1,20 @@
 import test from 'tape' // 单元测试的库
 import { incrementAsync } from './saga'
-import {delay} from 'redux-saga'
-import { call } from 'redux-saga/effects'
+import * as types from './action-types'
+import { delay, put, call } from 'redux-saga/effects'
 
 // 有两个参数 第一个是测试用例名，第二个是具体函数
 test('incrementAsync saga test', function(assert) {
   let gen = incrementAsync();
-  assert.deepEqual( // 深度比较两个返回值是否相等
+  let obj = {name: 'yang'}
+  assert.deepEqual( // 断言库 深度比较两个返回值是否相等
     gen.next().value,
-    call(delay,1000),
+    call([obj,delay], 1000),
     "第一次执行应该会返回一个延迟1秒的promise"
+  )
+  assert.deepEqual(
+    gen.next().value,
+    put({type: types.INCREMENT})
   )
   assert.end();
 });
