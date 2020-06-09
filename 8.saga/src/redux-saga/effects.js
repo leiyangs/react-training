7a330e05a1ffc838e83fea6ff71f2579f01b23ad
leiyangs <15135210985@163.com>
2020-06-09 18:04:33 +0800

    如果effect返回的是迭代器

--- 8.saga/src/redux-saga/effects.js
+++ 8.saga/src/redux-saga/effects.js
@@ -10,3 +10,7 @@ export function put(action) {
     action
   }
 }
+// takeEvery相当于要开启一个新的子进程，单独监听actionType，当动作发生时执行迭代器
+export function* takeEvery(actionType, generator) {
+  
+}
--- 8.saga/src/store/saga/counterSaga.js
+++ 8.saga/src/store/saga/counterSaga.js
@@ -1,9 +1,9 @@
-import { take,put } from '../../redux-saga/effects'
+import { all, take,put } from '../../redux-saga/effects'
 import * as types from '../action-types'
 function* increment() {
   yield put({type: types.INCREMENT})
 }
-export default function* () {
+export default function* asyncincreament() {
   for (let i = 0; i < 3; i++) {
     const action = yield take(types.ASYNC_INCREMENT);
     console.log('开始执行countersaga',action);
@@ -12,3 +12,9 @@ export default function* () {
   }
   alert('最多执行三次')
 }
+
+// function* () {
+//   yield all([
+//     asyncincreament()
+//   ])
+// }
