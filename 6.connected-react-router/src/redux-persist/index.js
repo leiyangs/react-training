a9c4a5aad89a8926dfa05e0c04cfb49108791cf5
leiyangs <15135210985@163.com>
2020-06-23 23:09:49 +0800

    redux-persist

--- 6.connected-react-router/src/redux-persist/index.js
+++ 6.connected-react-router/src/redux-persist/index.js
@@ -1,33 +1,3 @@
-const PERSIST_INIT = 'PERSIST_INIT';
-function persistStore(store) {
-  let persistor = {
-    ...store,
-    initState() {
-      store.dispatch({type: PERSIST_INIT})
-    }
-  }
-  return persistor;
-}
-function persistReducer(persistConfig, reducers) {
-  let key = `persist:${persistConfig.key}`;
-  let isInited = false;
-  return function (state, action) {
-    switch (action.type) {
-      case PERSIST_INIT:
-        isInited = true;
-        let value = persistConfig.storage.get(key);
-        state = JSON.parse(value);
-        return state;
-    
-      default:
-        if(isInited) {
-          state = reducers(state, action);
-          persistConfig.storage.set(key, JSON.stringify(state));
-        }else {
-          return reducers(state, action)
-        }
-        return state;
-    }
-  }
-}
+import persistStore from './persistStore'
+import persistReducer from './persistReducer'
 export { persistStore, persistReducer }
